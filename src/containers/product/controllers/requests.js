6509b5dcc19e9862ed8c1d6aa291af0419fd7ab2
HenkkaH <henri.hamalainen@hurja.fi>
2019-03-28 00:46:59 +0200

    Merge

--- src/containers/product/controllers/requests.js
+++ src/containers/product/controllers/requests.js
@@ -2,13 +2,8 @@ import { WEB_URL } from '../../../app/config'
 import Api from '../../../app/controllers/api'
 
 async function getProducts(props) {  
-<<<<<<< HEAD
   var response = await Api.get(WEB_URL + '/products', false);  
   console.log(response)
-=======
-  var response = await Api.get(WEB_URL + '/products');  
-
->>>>>>> aa7c35b9c30063faf28fa5c82d09995fd01490e9
   if(response){
     if(response.error) {
       return;  //TODO: return modal with the custom error msg or don't return the error at all.
