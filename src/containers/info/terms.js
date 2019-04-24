964eb0fd09b38fbc1904d361c405bd10a91790c3
Ville <villeih@student.uef.fi>
2019-04-24 13:17:08 +0300

    terms screen

--- src/containers/info/terms.js
+++ src/containers/info/terms.js
@@ -9,17 +9,17 @@ import CustomHeader from '../../app/components/header/customHeader'
 class Index extends Component {
 
     static navigationOptions = {
-		headerStyle: {
-			backgroundColor: theme.color.navigation.background,
-			height: theme.navigation.height,
-		},
-		headerTitle: <CustomHeader/>,
-		headerLeft: (
-			<View></View> //needed to justify logo in center
-		
-		  ),
-		
-	};
+        headerStyle: {
+            backgroundColor: theme.color.navigation.background,
+            height: theme.navigation.height,
+        },
+        headerTitle: <CustomHeader />,
+        headerLeft: (
+            <View></View> //needed to justify logo in center
+
+        ),
+
+    };
 
     constructor(props) {
         super(props);
@@ -41,7 +41,7 @@ class Index extends Component {
     }
 
     parseHtml(html) {
-        let str = html.replace('<p>[vc_row][vc_column][vc_column_text el_class=&#8221;tietosuojaseloste&#8221;]</p>', ' ')
+        let str = html.replace('<p>[vc_row][vc_column][vc_column_text el_class=&#8221;tietosuojaseloste&#8221;]</p>', '<br><br/>')
         str = str.replace(/[\u00e4]/g, '&#228').replace(/[\u00f6]/g, '&#246').replace('[/vc_column_text][/vc_column][/vc_row]', ' ')
         return str
     }
